ec5b67b9310dcf491f0305c6b753d0be3c190acd
Kai Hoffman <hoffm154@purdue.edu>
2017-06-05 14:11:33 -0400

    Change the paragraph when button pressed

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ function handleSubmit(ev) {
   ev.preventDefault()
   const f = ev.target // should give us the object submitted (the form)
   const name = f.personName.value
-  const color = f.faveColor.value
-  const newTitle = name + "'s favorite color is " + color;
+  const fcolor = f.faveColor.value
+
+  const newTitle = name + "'s favorite color is " + fcolor;
   document.querySelector('h1').textContent = newTitle // Changes heading 1
+
+  document.querySelector('p.emptyPara').textContent = name + " thinks that Xterns logo should be " + fcolor // Changes para
 }
 
 personForm.addEventListener('submit', handleSubmit)
