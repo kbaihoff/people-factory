def6e8b0b0d1f868a8633980d13f707d0fd921e5
Kai Hoffman <hoffm154@purdue.edu>
2017-06-05 13:43:42 -0400

    Change the heading when button pressed

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ function handleSubmit(ev) {
   ev.preventDefault()
   const f = ev.target // should give us the object submitted (the form)
   const name = f.personName.value
-  // const color
-  document.querySelector('h1').textContent = name // Changes heading 1
+  const color = f.faveColor.value
+  const newTitle = name + "'s favorite color is " + color;
+  document.querySelector('h1').textContent = newTitle // Changes heading 1
 }
 
 personForm.addEventListener('submit', handleSubmit)
